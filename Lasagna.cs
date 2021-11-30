58c23606ba1c702e853a285c6dcf90b1a031efa6
Victor Luiz <victorluizcefet@gmail.com>
2021-11-30 20:43:49 -0300

    exercise 1

---
+++ Lasagna.cs
@@ -0,0 +1,19 @@
+﻿class Lasagna
+{
+    public int ExpectedMinutesInOven() 
+    {
+        return 40;
+    }
+    public int RemainingMinutesInOven(int time)
+    {
+        return 40 - time;
+    }
+    public int PreparationTimeInMinutes(int layer)
+    {
+        return layer * 2;
+    }
+    public int ElapsedTimeInMinutes(int layer, int time)
+    {
+        return PreparationTimeInMinutes(layer) + time;
+    }
+}
