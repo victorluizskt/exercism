1ee702058169bb873e050c611c5ead0cd37d3b9e
Victor Luiz <victorluizcefet@gmail.com>
2021-11-30 21:26:10 -0300

    exercise 3

---
+++ LogLevels.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+static class LogLine
+{
+    public static string Message(string logLine)
+    {
+        return "Vazio";
+    }
+
+    public static string LogLevel(string logLine)
+    {
+        char second = logLine[1];
+        if(second == 'I')
+        {
+            return "info";
+        } 
+    
+        if(second == 'W')
+        {
+            return "warning";
+        }
+
+        return "error";
+    }
+
+    public static string Reformat(string logLine)
+    {
+        return "Operation completed (info)";
+    }
+}
