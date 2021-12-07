7de02df56b327238e52cf3951b282aa37e463ddf
Victor Luiz <victorluizcefet@gmail.com>
2021-12-06 21:47:13 -0300

    exercise 6

---
+++ PhoneNumberAnalysis.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public static class PhoneNumber
+{
+    public static (bool IsNewYork, bool IsFake, string LocalNumber) Analyze(string phoneNumber)
+    {
+        return (phoneNumber.Substring(0, 3).Equals("212"), 
+                phoneNumber.Substring(4, 3).Equals("555"), 
+                phoneNumber.Substring(8, 4)
+        );
+    }
+
+    public static bool IsFake((bool IsNewYork, bool IsFake, string LocalNumber) phoneNumberInfo)
+    {
+        return Analyze(
+            (phoneNumberInfo.IsNewYork ? "212-" : "000-") + 
+            (phoneNumberInfo.IsFake ? "555-" : "000-") + 
+            (phoneNumberInfo.LocalNumber)
+        ).Item2;
+    }
+}
